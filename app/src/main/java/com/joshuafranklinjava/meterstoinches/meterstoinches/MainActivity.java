1670688542be4373d97633b93e8d4a6c1de0b2ba
jfranklin1607 <jfranklin1607@gmail.com>
2018-07-31 22:21:21 -0400

    mobile-converter

---
+++ app/src/main/java/com/joshuafranklinjava/meterstoinches/meterstoinches/MainActivity.java
@@ -0,0 +1,54 @@
+package com.joshuafranklinjava.meterstoinches.meterstoinches;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+  /*
+    1m = 39.3701in
+   */
+
+  private EditText enterMeters;
+  private Button convertButton;
+  private TextView resultTextView;
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    enterMeters = (EditText) findViewById(R.id.metersEditText);
+    resultTextView = (TextView) findViewById(R.id.resultId);
+
+    convertButton = (Button) findViewById(R.id.convertButtonID);
+    convertButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+// Conversion Logic
+
+        double multiplier = 39.37;
+        double result = 0.0;
+
+        if (enterMeters.getText().toString().equals("")) {
+          resultTextView.setText(R.string.error_message);
+          resultTextView.setTextColor(Color.RED);
+        } else {
+
+          double meterValue = Double.parseDouble(enterMeters.getText().toString());
+          result = meterValue * multiplier;
+
+          resultTextView.setTextColor(Color.DKGRAY);
+          resultTextView.setText(String.format("%.2f", result) + " inches");
+        }
+      }
+
+
+    });
+  }
+}
